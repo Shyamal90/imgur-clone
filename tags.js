be21d9e59012007a3abd6fbb5cc31f548c24043a
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-03-06 10:00:27 +0530

    second commit

---
+++ tags.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        id: 1,
+        img: "https://i.imgur.com/uJSrScD_d.jpg",
+        title_name: "Space",
+        posts: "28,305",
+        bg_color:"rgb(50, 141, 103)"
+    }
+
+]
+
+/*
+                    <a class="Tag " href="/t/vintage?source=featured_tag_module"
+                        style="background-image: url(&quot;https://i.imgur.com/uJSrScD_d.jpg?maxwidth=800&amp;shape=thumb&amp;fidelity=high&quot;); height: 130px; width: 110px;">
+                        <div class="Tag-title" style="background-color: rgb(50, 141, 103);">
+                            <div class="Tag-name">vintage</div>
+                            <div class="Tag-posts">8,931 Posts</div>
+                        </div>
+                    </a>
+*/
